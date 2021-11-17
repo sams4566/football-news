2f704a0a38d2f870e594bc08aa4057d6d0f69495
sams4566 <sams4566@gmail.com>
2021-11-17 12:41:15 +0000

    Add tests for functions in view.py

--- news/tests.py
+++ news/tests.py
@@ -4,18 +4,39 @@ from .forms import CategoryForm
 
 class TestCategoryModel(TestCase):
 
-    def test_category_string_returns_category_name(self):
+    def test_category_string_gives_back_category_name(self):
         category = Category.objects.create(category_name='Test String')
         self.assertEqual(str(category), 'Test String')
 
-    def test_approve_category_defaults_to_false(self):
+    def test_approve_category_set_to_false(self):
         category = Category.objects.create(category_name='Test String')
         self.assertFalse(category.approve_category)
 
 class TestCategoryForm(TestCase):
 
-    def test_category_name_is_required(self):
+    def test_category_name_is_needed(self):
         form = CategoryForm({'category_name': ''})
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['category_name'][0], 'This field is required.')
-    
+
+class TestViews(TestCase):
+
+    def test_retrieve_category_list(self):
+        page = self.client.get('/categories')
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, 'categories.html')
+
+    def test_retrieve_add_category_html(self):
+        page = self.client.get('/category/add')
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, 'add_category.html')
+
+    def test_retrieve_edit_category_html(self):
+        category = Category.objects.create(category_name='Test String')
+        page = self.client.get(f'/category/edit/{category.id}')
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, 'edit_category.html')
+
+    def test_category_added(self):
+        page = self.client.post('/category/add', {'category_name': 'Test String'})
+        self.assertRedirects(page, '/categories')
