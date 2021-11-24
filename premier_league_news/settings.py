d7d80837555a06a3a4b7080f2ed63de139c6e71e
sams4566 <sams4566@gmail.com>
2021-11-24 13:49:16 +0000

    Set up database for local host

--- premier_league_news/settings.py
+++ premier_league_news/settings.py
@@ -18,7 +18,7 @@ import os
 if os.path.isfile('env.py'):
     import env
 
-# development = os.environ.get('DEVELOPMENT', False)
+development = os.environ.get('DEVELOPMENT', False)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -32,17 +32,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = development 
 
-ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+# ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 # ALLOWED_HOSTS = ['premier-league-news1.herokuapp.com', 'localhost']
 
 # DEBUG = development 
 
-# if development:
-#     ALLOWED_HOSTS = ['localhost']
-# else:
-#     ALLOWED_HOSTS = [os.environ.get('premier-league-news1.herokuapp.com')]
+if development:
+    ALLOWED_HOSTS = ['localhost']
+else:
+    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 # DEBUG = 'DEVELOPMENT' in os.environ
 
@@ -114,18 +115,18 @@ WSGI_APPLICATION = 'premier_league_news.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# if development:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'db.sqlite3',
-#         }
-#     }
+if development:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
-# else:
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
 
 # if os.environ.get("DATABASE_URL"):
 #     DATABASES = {
