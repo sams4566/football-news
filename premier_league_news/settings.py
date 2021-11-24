482b5abcbd8023158f6c5459655f2e470ee6be35
sams4566 <sams4566@gmail.com>
2021-11-24 09:47:59 +0000

    Change setting to deploy to Heroku

--- premier_league_news/settings.py
+++ premier_league_news/settings.py
@@ -18,7 +18,7 @@ import os
 if os.path.isfile('env.py'):
     import env
 
-development = os.environ.get('DEVELOPMENT', True)
+development = os.environ.get('DEVELOPMENT', False)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
@@ -91,7 +91,7 @@ ROOT_URLCONF = 'premier_league_news.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [TEMPLATES_DIR], # TEMPLATES_DIR
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
